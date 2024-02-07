91f2444a07d3c860c5b2b15f49ea122e859d6c39
Etyre <elityre@gmail.com>
2024-02-06 19:27:40 -0800

    solved our problem where the cursor kept going to the front and or disapearing every time we typed a character.

--- version_1/frontend/src/components/Typingbox/index.jsx
+++ version_1/frontend/src/components/Typingbox/index.jsx
@@ -1,35 +1,74 @@
-import {useEffect, useState} from "react"
+import {useEffect, useState, useRef} from "react"
 
+
+function typedTextParser (text) {
+    arrayOfWords = text.split(" ")
+    arrayOfWordsWithSpaces = []
+    for (let index = 0; index < arrayOfWords.length; index++) {
+        // We're adding a space to the end of each element, except for the last one.
+        let element
+        if(index == arrayOfWords.length - 1){
+            element = arrayOfWords[index]}
+        else{
+            element = arrayOfWords[index]+" ";}
+        arrayOfWordsWithSpaces.push(element)
+    }
+    return arrayOfWordsWithSpaces
+}
+
+function ContentEditableWithRef (props) {
+    const defaultValue = useRef(props.value);
+
+    const handleInput = (event) => {
+        props.onChange(event.target.innerHTML);
+    };
+
+    return (
+    <div
+        className="editableSection"
+        contentEditable="true"
+        onInput={handleInput}
+        dangerouslySetInnerHTML={{ __html: defaultValue.current }}
+            //This line right here is just for the initial value, in case I want to pass in something other than the empty string.
+    />
+    );
+};
 
 export default function Textbox ({textToType}) {
 // An alternative way to write this line:
 // export default function Textbox (props) 
 
-const [text, setText] = useState("")
-const [isValid, setIsValid] = useState(true)
+    const [text, setText] = useState("")
+    const [isValid, setIsValid] = useState(true)
 
-useEffect(()=> {
-    console.log("textToType is: ",textToType)
-    console.log("text is: ",text)
+    useEffect(()=> {
+        console.log("textToType is: ",textToType)
+        console.log("text is: ", `"${text}"`)
 
-    if(textToType.startsWith(text)){
-        setIsValid(true)
-    }else{
-        setIsValid(false)
+        // if()
+
+        if(textToType.startsWith(text)){
+            setIsValid(true)
+        }else{
+            setIsValid(false)
+        }
+    }, [text])
+
+    function handleText(event) {
+        setText(event.target.textContent.replaceAll(String.fromCharCode(160), " "))
+        // We're doing the replaceAll because the contentEditable div by default uses a different type of space from the standard text space (for trailing spaces only), and we need it to match textToType.
+
     }
-}, [text])
 
-
-function handleText(event) {
-    setText(event.target.value)
-}
-
-return(
-    <div className="typingBox">
-        <textarea className={isValid==false? "redTextArea": ""} onChange={handleText} />
-        <div className="editableSection" contentEditable="true">
-            <p>Text text to see the div</p>
+    return(
+        <div className="typingBox">
+            <ContentEditableWithRef value={text} 
+            onChange={(newValue) => { setText(newValue) }}
+            />
+            {/* <textarea className={isValid==false? "redTextArea": ""} onChange={handleText} /> */}
+            {/* <div onInput={handleText} className={"editableSection " + (isValid==false? "redTextArea": "")} dangerouslySetInnerHTML={{__html: text}} contentEditable="true">
+                
+            </div> */}
         </div>
-    </div>
-)
-}
+    )
+    }
