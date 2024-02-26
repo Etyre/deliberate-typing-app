e3fc1e4fa38e95ac67bfb653d0fe350b1034e049
Etyre <elityre@gmail.com>
2024-02-25 21:06:41 -0800

    made a Dockerfile for postgres

--- version_1/backend/index.js
+++ version_1/backend/index.js
@@ -1,12 +1,17 @@
 import express from "express"
+import path from "path"
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// Got this from here: https://stackoverflow.com/a/50052194
 
 const app = express()
 
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hi!')
-})
+app.use('/', express.static(path.join(__dirname, '../frontend/dist')))
+// This doesn't seem to work with "get", even though when we use "use", that uses a get requset. It seems to work with "use" though ¯\_(ツ)_/¯.
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
