fd84b91bf8fd1d8a263dd66257b2bd1a2ca1f613
Etyre <elityre@gmail.com>
2024-06-13 19:32:29 -0700

    Adding an if clause to our generate-private-key.sh that checks if the private key is already there, before we recreate it.

--- version_1/backend/scripts/generate-private-key.sh
+++ version_1/backend/scripts/generate-private-key.sh
@@ -1,4 +1,7 @@
-mkdir -p ./secrets
+if [ -e ./secrets/jwtRS256.key ]; then
+  echo "Private key already exists"
+  exit 0
+fi
 ssh-keygen -t rsa -b 4096 -m PEM -f ./secrets/jwtRS256.key -q -N ""
 # Don't add passphrase
 openssl rsa -in ./secrets/jwtRS256.key -pubout -outform PEM -out ./secrets/jwtRS256.key.pub
